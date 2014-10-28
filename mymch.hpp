b224334ae2670745aea4161e0724c4929faba19a
Konstantin Neumoin <kneumoin@parallels.com>
2014-10-28 13:45:21 +0400

    add function table

--- mymch.hpp
+++ mymch.hpp
@@ -6,10 +6,11 @@
 #include <cstdlib>
 #include <stack>
 #include <time.h>
+#include <map>
 
 #define VERSION 0.1
 
-#define DEBUG
+//#define DEBUG
 
 using namespace std;
 
@@ -18,12 +19,8 @@ void resize_line(long int*& old_one, int new_size, long int z) {
 	old_one = &old_one[-1];
 	int i, old_size = static_cast<int>(old_one[0]);
 	new_one[0] = new_size;
-	// TO_DO VVV
-	for (i = 1; i <= old_size; ++i){
-		new_one[i] = old_one[i];
-	}
-	for (; i < new_size; ++i){
-		new_one[i] = z;
+	for (i = 1; i < new_size; ++i){
+		new_one[i] = (i <= old_size) ? old_one[i] : z;
 	}
 	delete[] old_one;
 	old_one = new_one;
@@ -34,15 +31,15 @@ int get_line_size(long int* line) {
 	return line[0];
 }
 
-class BefungeStackMachine {
+class Context {
 	stack<long int> m_stack;
+public:
 	unsigned int width;
 	unsigned int length;
 	unsigned int point[2];
-	vector<long int*> program;
-	char cur_dir;
-	bool running;
+	long int cur_dir;
 	bool symb_mode;
+	bool running;
 	long int pop() {
 #ifdef DEBUG
 		cout << point[0] << 'x' << point[1] << " pop: ";
@@ -70,76 +67,95 @@ class BefungeStackMachine {
 		cout << point[0] << 'x' << point[1] << " push: " << c << endl;
 #endif
 	}
+};
+
+typedef int (*Function)(Context*);
+typedef map<long int, Function> function_table;
+
+int move_r(Context* cont){
+	++cont->point[1];
+	if (cont->width and cont->point[1] >= cont->width)
+		cont->point[1] = cont->point[1] - cont->width;
+	return 0;
+}
+
+int move_l(Context* cont){
+	--cont->point[1];
+	if (cont->width and cont->point[1] < 0)
+		cont->point[1] = cont->width - cont->point[1];
+	return 0;
+}
+
+int move_d(Context* cont){
+	++cont->point[0];
+	if (cont->length and cont->point[0] >= cont->length)
+		cont->point[0] = cont->point[0] - cont->length;
+	return 0;
+}
+
+int move_u(Context* cont){
+	--cont->point[0];
+	if (cont->length and cont->point[0] < 0)
+		cont->point[0] = cont->length - cont->point[0];
+	return 0;
+}
+
+class BefungeStackMachine {
+	vector<long int*> program;
+	Context cont;
+	function_table ft;
 
 	int move(){
-		switch(cur_dir) {
-			case '>':
-				++point[1];
-				if (width and point[1] >= width)
-					point[1] = point[1] - width;
-				break;
-			case '<':
-				--point[1];
-				if (width and point[1] < 0)
-					point[1] = width - point[1];
-				break;
-			case 'v':
-				++point[0];
-				if (length and point[0] >= length)
-					point[0] = point[0] - length;
-				break;
-			case '^':
-				--point[0];
-				if (length and point[0] < 0)
-					point[0] = length - point[0];
-				break;
-			default:
-				return -1;
-			}
 #ifdef DEBUG
 		usleep(150000);
 #endif
-		return 0;
+		try {
+			return ft[cont.cur_dir](&cont);
+		}
+		catch(...) {
+			return -1;
+		}
 	}
 
 	long int getsymbol() {
 		int new_size;
-		if (point[0] >= program.size()){
-			if (length)
-				new_size = length;
+		if (cont.point[0] >= program.size()){
+			if (cont.length)
+				new_size = cont.length;
 			else
-				new_size = point[0] * 1.5;
+				new_size = cont.point[0] * 1.5;
 			program.resize(new_size);
 		}
-		if (point[1] >= get_line_size(program[point[0]])) {
-			if (length)
-				new_size = length;
+		if (cont.point[1] >= get_line_size(program[cont.point[0]])) {
+			if (cont.length)
+				new_size = cont.length;
 			else
-				new_size = point[1] * 1.5;
-			resize_line(program[point[0]], new_size, static_cast<long int>(' '));
+				new_size = cont.point[1] * 1.5;
+			resize_line(program[cont.point[0]], new_size, static_cast<long int>(' '));
 		}
 #ifdef DEBUG
-		cout << "c: " << program[point[0]][point[1]] << "(" << static_cast<char>(program[point[0]][point[1]]) << ")" << endl;
+		cout << "c: " << program[cont.point[0]][cont.point[1]] << \
+			"(" << static_cast<char>(program[cont.point[0]][cont.point[1]]) << ")" << endl;
 #endif
-		return program[point[0]][point[1]];
+		return program[cont.point[0]][cont.point[1]];
 	}
 
 	int exec(){
 		long int a, b, c = getsymbol();
 		char x[2] = {0};
 
-		if (symb_mode && c != '"') {
-			push(c);
+		if (cont.symb_mode && c != '"') {
+			cont.push(c);
 			return 0;
 		}
 
 		if (c == '>' or c == '<' or c == 'v' or c == '^') {
-			cur_dir = c;
+			cont.cur_dir = c;
 			return 0;
 		}
 
 		if (c == ',') {
-			a = pop();
+			a = cont.pop();
 #ifdef DEBUG
 			cout << "print: '";
 #endif
@@ -151,7 +167,7 @@ class BefungeStackMachine {
 		}
 
 		if (c == '.') {
-			a = pop();
+			a = cont.pop();
 #ifdef DEBUG
 			cout << "print: '";
 #endif
@@ -166,106 +182,113 @@ class BefungeStackMachine {
 			case ' ':
 				return 0;
 			case '"':
-				symb_mode = not symb_mode;
+				cont.symb_mode = not cont.symb_mode;
 				return 0;
 			case '@':
-				running = false;
+				cont.running = false;
 				return -1;
 			case '?':
-				cur_dir = "v^<>"[rand() % 4];
+				cont.cur_dir = "v^<>"[rand() % 4];
 				return 0;
 			case '*':
-				push(pop() * pop());
+				cont.push(cont.pop() * cont.pop());
 				return 0;
 			case '+':
-				push(pop() + pop());
+				cont.push(cont.pop() + cont.pop());
 				return 0;
 			case 'p':
-				program[pop()][pop()] = pop();
+				program[cont.pop()][cont.pop()] = cont.pop();
 				return 0;
 			case 'g':
-				push(program[pop()][pop()]);
+				cont.push(program[cont.pop()][cont.pop()]);
 				return 0;
 			case '_':
-				if (pop())
-					cur_dir = '<';
+				if (cont.pop())
+					cont.cur_dir = '<';
 				else
-					cur_dir = '>';
+					cont.cur_dir = '>';
 				return 0;
 			case '|':
-				if (pop())
-					cur_dir = '^';
+				if (cont.pop())
+					cont.cur_dir = '^';
 				else
-					cur_dir = 'v';
+					cont.cur_dir = 'v';
 				return 0;
 			case '#':
 				move();
 				return 0;
 			case '!':
-				push(static_cast<long int>(not static_cast<bool>(pop())));
+				cont.push(static_cast<long int>(not static_cast<bool>(cont.pop())));
 				return 0;
 			case '-':
-				a = pop();
-				b = pop();
-				push(b - a);
+				a = cont.pop();
+				b = cont.pop();
+				cont.push(b - a);
 				return 0;
 			case '/':
-				a = pop();
+				a = cont.pop();
 				if (!a){
 					cout << "divide by zero" << endl;
 					return -1;
 				}
-				b = pop();
-				push(b / a);
+				b = cont.pop();
+				cont.push(b / a);
 			case '%':
-				a = pop();
+				a = cont.pop();
 				if (!a){
 					cout << "divide by zero" << endl;
 					return -1;
 				}
-				b = pop();
-				push(b % a);
+				b = cont.pop();
+				cont.push(b % a);
 				return 0;
 			case '$':
-				pop();
+				cont.pop();
 				return 0;
 			case '\\':
-				a = pop();
-				b = pop();
-				push(a);
-				push(b);
+				a = cont.pop();
+				b = cont.pop();
+				cont.push(a);
+				cont.push(b);
 				return 0;
 			case '`':
-				a = pop();
-				b = pop();
-				push(static_cast<long int>(b > a));
+				a = cont.pop();
+				b = cont.pop();
+				cont.push(static_cast<long int>(b > a));
 				return 0;
 			case '~':
 				char x;
 				cin >> x;
-				push(static_cast<long int>(x));
+				cont.push(static_cast<long int>(x));
 				return 0;
 			case '&':
 				cin >> a;
-				push(a);
+				cont.push(a);
 				return 0;
 			case ':':
-				a = pop();
-				push(a);
-				push(a);
+				a = cont.pop();
+				cont.push(a);
+				cont.push(a);
 				return 0;
 			}
-		push(c - '0');
+		cont.push(c - '0');
 		return 0;
 	}
 public:
-	BefungeStackMachine(ifstream& fin, int l = 0, int w = 0) : length(l), width(w) {
-		if (length or width) {
+	BefungeStackMachine(ifstream& fin, long int l = 0, long int w = 0) {
+		cont.length = l;
+		cont.width = w;
+		if (cont.length or cont.width) {
 			//TO_DO 25x80
 			throw;
 		}
-		running = false;
-		symb_mode = false;
+		cont.running = false;
+		cont.symb_mode = false;
+		ft.insert(make_pair((long int)'>', &move_r));
+		ft.insert(make_pair((long int)'<', &move_l));
+		ft.insert(make_pair((long int)'^', &move_u));
+		ft.insert(make_pair((long int)'v', &move_d));
+
 		srand(time(NULL));
 		long int *converted_line;
 
@@ -290,16 +313,16 @@ public:
 	}
 
 	int run(const int* start_point){
-		running = true;
-		memcpy(point, start_point, sizeof(start_point));
-		cur_dir = '>';
+		cont.running = true;
+		memcpy(cont.point, start_point, sizeof(start_point));
+		cont.cur_dir = '>';
 		do{
 			if (exec() or move())
-				running = false;
+				cont.running = false;
 #ifdef DEBUG
 			cout << endl;
 #endif
-		} while(running);
+		} while(cont.running);
 	}
 };
 
